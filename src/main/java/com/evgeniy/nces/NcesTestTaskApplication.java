ea7b22cdf9cb43780be45ca924b653bc77d550ae
Evgeniy Leshevich <evgeniy.le99@gmail.com>
2022-10-31 15:55:52 +0300

    Initial commit

---
+++ src/main/java/com/evgeniy/nces/NcesTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.evgeniy.nces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcesTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcesTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evgeniy/nces/NcesTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evgeniy.nces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcesTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
